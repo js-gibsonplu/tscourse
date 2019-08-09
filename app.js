f3e6eb7cede6bccc295da0da3ec867aa772529a6
Jonathan Sexton <jsexton@gibsonplumbing.com>
2019-08-09 12:32:55 -0500

    work

--- app.js
+++ app.js
@@ -1,2 +1,6 @@
 "use strict";
 console.log('it compiles correctly');
+var number = 38;
+console.log(number);
+number = 1;
+console.log(number);
--- app.ts
+++ app.ts
@@ -1,1 +1,4 @@
 console.log('it compiles correctly')
+
+let number = 38; console.log(number);
+number = 1; console.log(number);
