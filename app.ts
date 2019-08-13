3368d99635b525b09e5e5e47706345ff72508eb2
Jonathan Sexton <jsexton@gibsonplumbing.com>
2019-08-13 07:47:41 -0500

    learning things

--- app.ts
+++ app.ts
@@ -2,3 +2,14 @@ console.log('it compiles correctly')
 
 let number = 38; console.log(number);
 number = 1; console.log(number);
+
+let age:number;
+age = 29;
+console.log(age);
+age = 34;
+console.log(age);
+
+let names: any[] = ['james', 'billy','steve']
+console.log(names);
+names[0] = 100;
+console.log(names);
