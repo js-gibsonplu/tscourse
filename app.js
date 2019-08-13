c74d298cff77e1ebf16735c6f2ee160d134e1836
Jonathan Sexton <jsexton@gibsonplumbing.com>
2019-08-13 14:01:33 -0500

    changes

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 "use strict";
-var myName = 'jonathan';
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
+// strings
+var myName = "jonathan";
+// myName = 17; cannot assign number to string type
 var animals = {
-    cat: 'lion',
-    dog: 'hyena',
-    snake: 'boa'
+    cat: "lion",
+    dog: "hyena",
+    snake: "boa"
 };
 var newNum = 19191919191;
+// numbers
 var number = 38;
 console.log(number);
 number = 1;
@@ -15,10 +29,13 @@ age = 29;
 console.log(age);
 age = 34;
 console.log(age);
-var names = ['james', 'billy', 'steve'];
+// arrays
+var names = ["james", "billy", "steve"];
 console.log(names);
-var mine = [12, 'twelve', true];
+// tuples
+var mine = [12, "twelve", true];
 console.log(mine);
+// enums
 var namesEnum;
 (function (namesEnum) {
     namesEnum[namesEnum["jonathan"] = 16] = "jonathan";
@@ -29,9 +46,32 @@ var myNamesEnum = namesEnum.john;
 console.log(myNamesEnum);
 console.log(namesEnum);
 console.log(namesEnum[17]);
+// functions
 function returnMyAge() {
     return newNum;
 }
 console.log("my return function " + returnMyAge());
 var multiply = returnMyAge;
 console.log(multiply());
+// objects
+// let data = {
+//   name: "jon",
+//   age: 34
+// };
+// console.table(data);
+// data = {
+//   age: 34,
+//   name: "jon"
+// };
+// console.table(data);
+var userData = {
+    name: "bill",
+    age: 8665857
+};
+console.table(userData);
+var userData2 = __assign({}, userData);
+userData2 = {
+    a: 17,
+    n: 'bacon',
+};
+console.table(userData2);
--- app.ts
+++ app.ts
@@ -1,38 +1,40 @@
 // strings
-let myName = 'jonathan';
+let myName = "jonathan";
 // myName = 17; cannot assign number to string type
 
 let animals = {
-    cat : 'lion',
-    dog : 'hyena',
-    snake : 'boa'
-}
+  cat: "lion",
+  dog: "hyena",
+  snake: "boa"
+};
 
 const newNum = 19191919191;
 
 // numbers
-let number = 38; console.log(number);
-number = 1; console.log(number);
+let number = 38;
+console.log(number);
+number = 1;
+console.log(number);
 
-let age:number;
+let age: number;
 age = 29;
 console.log(age);
 age = 34;
 console.log(age);
 
 // arrays
-let names: string[] = ['james', 'billy','steve']
+let names: string[] = ["james", "billy", "steve"];
 console.log(names);
 
 // tuples
-let mine: [number, string, boolean]= [12, 'twelve', true];
-console.log(mine)
+let mine: [number, string, boolean] = [12, "twelve", true];
+console.log(mine);
 
 // enums
 enum namesEnum {
-    'jonathan' = 16,
-    'nic',
-    'john'
+  "jonathan" = 16,
+  "nic",
+  "john"
 }
 
 let myNamesEnum: namesEnum = namesEnum.john;
@@ -41,8 +43,8 @@ console.log(namesEnum);
 console.log(namesEnum[17]);
 
 // functions
-function returnMyAge() :number {
-    return newNum;
+function returnMyAge(): number {
+  return newNum;
 }
 
 console.log(`my return function ` + returnMyAge());
@@ -52,3 +54,27 @@ let multiply: () => number = returnMyAge;
 console.log(multiply());
 
 // objects
+// let data = {
+//   name: "jon",
+//   age: 34
+// };
+// console.table(data);
+// data = {
+//   age: 34,
+//   name: "jon"
+// };
+// console.table(data);
+
+let userData: { name: string; age: number } = {
+  name: "bill",
+  age: 8665857
+};
+
+console.table(userData)
+
+let userData2: {name: string, age: number} = {...userData};
+userData2 = {
+    a: 17,
+    n: 'bacon',
+}
+console.table(userData2);
