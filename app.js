df14516c60cd9893a4554844b59c7198396097e9
Jonathan Sexton <joncsexton@.com>
2019-12-30 13:15:49 -0600

    added article examples

--- app.js
+++ app.js
@@ -1,77 +1,68 @@
-"use strict";
-var __assign = (this && this.__assign) || function () {
-    __assign = Object.assign || function(t) {
-        for (var s, i = 1, n = arguments.length; i < n; i++) {
-            s = arguments[i];
-            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
-                t[p] = s[p];
-        }
-        return t;
-    };
-    return __assign.apply(this, arguments);
-};
+/* eslint-disable no-unused-var*/
+/* eslint-disable no-console*/
 // strings
-var myName = "jonathan";
-// myName = 17; cannot assign number to string type
-var animals = {
-    cat: "lion",
-    dog: "hyena",
-    snake: "boa"
-};
-var newNum = 19191919191;
-// numbers
-var number = 38;
-console.log(number);
-number = 1;
-console.log(number);
-var age;
-age = 29;
-console.log(age);
-age = 34;
-console.log(age);
-// arrays
-var names = ["james", "billy", "steve"];
-console.log(names);
-// tuples
-var mine = [12, "twelve", true];
-console.log(mine);
-// enums
-var namesEnum;
-(function (namesEnum) {
-    namesEnum[namesEnum["jonathan"] = 16] = "jonathan";
-    namesEnum[namesEnum["nic"] = 17] = "nic";
-    namesEnum[namesEnum["john"] = 18] = "john";
-})(namesEnum || (namesEnum = {}));
-var myNamesEnum = namesEnum.john;
-console.log(myNamesEnum);
-console.log(namesEnum);
-console.log(namesEnum[17]);
-// functions
-function returnMyAge() {
-    return newNum;
-}
-console.log("my return function " + returnMyAge());
-var multiply = returnMyAge;
-console.log(multiply());
-// objects
-// let data = {
-//   name: "jon",
-//   age: 34
+var userName = "Jonathan";
+console.log("The user's name is " + userName);
+userName = 17;
+console.log("The user's name is " + userName);
+// let animals = {
+//   cat: "lion",
+//   dog: "hyena",
+//   snake: "boa"
 // };
-// console.table(data);
-// data = {
-//   age: 34,
-//   name: "jon"
+// const newNum = 19191919191;
+// // numbers
+// let number = 38;
+// console.log(number);
+// number = 1;
+// console.log(number);
+// let age: number;
+// age = 29;
+// console.log(age);
+// age = 34;
+// console.log(age);
+// // arrays
+// let names: string[] = ["james", "billy", "steve"];
+// console.log(names);
+// // tuples
+// let mine: [number, string, boolean] = [12, "twelve", true];
+// console.log(mine);
+// // enums
+// enum namesEnum {
+//   "jonathan" = 16,
+//   "nic",
+//   "john"
+// }
+// let myNamesEnum: namesEnum = namesEnum.john;
+// console.log(myNamesEnum);
+// console.log(namesEnum);
+// console.log(namesEnum[17]);
+// // functions
+// function returnMyAge(): number {
+//   return newNum;
+// }
+// console.log(`my return function ` + returnMyAge());
+// let multiply: () => number = returnMyAge;
+// console.log(multiply());
+// // objects
+// // let data = {
+// //   name: "jon",
+// //   age: 34
+// // };
+// // console.table(data);
+// // data = {
+// //   age: 34,
+// //   name: "jon"
+// // };
+// // console.table(data);
+// let userData: { name: string; age: number } = {
+//   name: "bill",
+//   age: 8665857
 // };
-// console.table(data);
-var userData = {
-    name: "bill",
-    age: 8665857
-};
-console.table(userData);
-var userData2 = __assign({}, userData);
-userData2 = {
-    a: 17,
-    n: 'bacon',
-};
-console.table(userData2);
+// console.table(userData)
+// let userData2: { name: string, age: number } = { ...userData };
+// userData2 = {
+//   a: 17,
+//   n: 'bacon',
+// }
+// console.table(userData2);
--- app.ts
+++ app.ts
@@ -8,79 +8,79 @@ userName = 17;
 console.log(`The user's name is ${userName}`)
 
 
-let animals = {
-  cat: "lion",
-  dog: "hyena",
-  snake: "boa"
-};
-
-const newNum = 19191919191;
-
-// numbers
-let number = 38;
-console.log(number);
-number = 1;
-console.log(number);
-
-let age: number;
-age = 29;
-console.log(age);
-age = 34;
-console.log(age);
-
-// arrays
-let names: string[] = ["james", "billy", "steve"];
-console.log(names);
-
-// tuples
-let mine: [number, string, boolean] = [12, "twelve", true];
-console.log(mine);
-
-// enums
-enum namesEnum {
-  "jonathan" = 16,
-  "nic",
-  "john"
-}
-
-let myNamesEnum: namesEnum = namesEnum.john;
-console.log(myNamesEnum);
-console.log(namesEnum);
-console.log(namesEnum[17]);
-
-// functions
-function returnMyAge(): number {
-  return newNum;
-}
-
-console.log(`my return function ` + returnMyAge());
-
-let multiply: () => number = returnMyAge;
-
-console.log(multiply());
-
-// objects
-// let data = {
-//   name: "jon",
-//   age: 34
+// let animals = {
+//   cat: "lion",
+//   dog: "hyena",
+//   snake: "boa"
 // };
-// console.table(data);
-// data = {
-//   age: 34,
-//   name: "jon"
+
+// const newNum = 19191919191;
+
+// // numbers
+// let number = 38;
+// console.log(number);
+// number = 1;
+// console.log(number);
+
+// let age: number;
+// age = 29;
+// console.log(age);
+// age = 34;
+// console.log(age);
+
+// // arrays
+// let names: string[] = ["james", "billy", "steve"];
+// console.log(names);
+
+// // tuples
+// let mine: [number, string, boolean] = [12, "twelve", true];
+// console.log(mine);
+
+// // enums
+// enum namesEnum {
+//   "jonathan" = 16,
+//   "nic",
+//   "john"
+// }
+
+// let myNamesEnum: namesEnum = namesEnum.john;
+// console.log(myNamesEnum);
+// console.log(namesEnum);
+// console.log(namesEnum[17]);
+
+// // functions
+// function returnMyAge(): number {
+//   return newNum;
+// }
+
+// console.log(`my return function ` + returnMyAge());
+
+// let multiply: () => number = returnMyAge;
+
+// console.log(multiply());
+
+// // objects
+// // let data = {
+// //   name: "jon",
+// //   age: 34
+// // };
+// // console.table(data);
+// // data = {
+// //   age: 34,
+// //   name: "jon"
+// // };
+// // console.table(data);
+
+// let userData: { name: string; age: number } = {
+//   name: "bill",
+//   age: 8665857
 // };
-// console.table(data);
 
-let userData: { name: string; age: number } = {
-  name: "bill",
-  age: 8665857
-};
+// console.table(userData)
 
-console.table(userData)
-
-let userData2: { name: string, age: number } = { ...userData };
-userData2 = {
-  a: 17,
-  n: 'bacon',
-}
-console.table(userData2);
+// let userData2: { name: string, age: number } = { ...userData };
+// userData2 = {
+//   a: 17,
+//   n: 'bacon',
+// }
+// console.table(userData2);
